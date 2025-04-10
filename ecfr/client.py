ba0ab3468136d2bdd07dabff87669db130f4cdcc
Ryan Baker <rbaker@xtier.com>
2025-04-10 06:33:49 -0400

    implement word counting, quite suboptimal at the moment

--- ecfr/client.py
+++ ecfr/client.py
@@ -2,6 +2,7 @@ import requests
 import json
 from datetime import datetime
 from agency import Agency
+import xml.etree.ElementTree as ET
 
 
 BASE_URL = "https://www.ecfr.gov"
@@ -33,12 +34,22 @@ def versioner_structure(date: datetime, title: int) -> dict:
 def versioner_full_url(date: datetime, title: int) -> str:
     return f"{BASE_URL}/api/versioner/v1/full/{date.strftime('%Y-%m-%d')}/title-{title}.xml"
 
-def versioner_full(date: datetime, title: int) -> str:
+def versioner_full(date: datetime, title: int) -> ET.Element:
     response = requests.get(versioner_full_url(date, title))
     response.raise_for_status()
-    print(response.text)
+    return ET.fromstring(response.content)
+
+def text_in_chapter(root: ET.Element, chapter: str) -> str:
+    chapter = root.find(f".//DIV3[@N='{chapter}']")
+    paragraphs = chapter.findall(".//P")
+    paragraphs = [("".join(p.itertext()).strip()) for p in paragraphs]
+    text = '\n'.join(paragraphs)
+    return text
+
+def text_from_agency(agency: Agency) -> str:
+    chapter_texts = [text_in_chapter(versioner_full(datetime(2025, 1, 20), ref["title"]), ref["chapter"]) for ref in agency.cfr_references]
+    return '\n'.join(chapter_texts)
 
 
 if __name__ == "__main__":
-    agencies = fetch_agencies()
-    print(count_agencies(agencies))
+    pass
